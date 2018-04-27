533d5bb1639d0995039fa19b792677b61cf02135
liangyea <332770873@qq.com>
2018-04-27 17:02:58 +0800

    add comment

--- src/main/java/com/springboot/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/springboot/springbootdemo/SpringbootdemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+//@SpringBootApplication是Spring Boot 项目的核心注解，
+// 主要目的是开启自动配置。main方法是一个标准的Java应用的main方法，
+// 主要作用是作为项目启动的入口。
 public class SpringbootdemoApplication {
 
 	@RequestMapping("/")
