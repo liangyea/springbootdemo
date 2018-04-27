16b39b5e949ad2f0d99834b0d50f23fab21451b4
liangyea <332770873@qq.com>
2018-04-27 16:53:47 +0800

    first commit

---
+++ src/main/java/com/springboot/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+	@RequestMapping("/")
+	String index() {
+		return "Hello SpringBoot";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+}
